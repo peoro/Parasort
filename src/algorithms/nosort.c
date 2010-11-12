46a44940af1e942bfa93aaf465369d8ff5ef1f81
Paolo Giangrandi <paolo@luccalug.it>
2010-11-12 11:14:21 +0000

    Added CFLAGS to ALGOCFLAGS in Makefile: now algorithms are compiled with -Wall and -Wextra

--- src/algorithms/nosort.c
+++ src/algorithms/nosort.c
@@ -7,9 +7,23 @@
 
 void mainSort( const TestInfo *ti, int *array, long size )
 {
-	printf( "Should sort an array of %ld elements...\n", size );
+	// printf( "Should sort an array of %ld elements...\n", size );
+	
+	int step = 0;
+	
+	if( GET_ID(ti) == ti->algoVar[0] ) {
+		for( step = 0; step < GET_STEP_COUNT(ti); ++ step ) {
+			if( do_i_receive( ti, step ) ) {
+				printf( "%d @ step %d :: recveiving from %d\n", from_who( ti, step ) );
+			}
+			else if( do_i_send( ti, step ) ) {
+				printf( "%d @ step %d :: sending to %d\n", to_who( ti, step ) );
+			}
+		}
+	}
 }
 void sort( const TestInfo *ti )
 {
-	printf( "Should sort an array...\n" );
+	// printf( "Should sort an array...\n" );
+	mainSort( ti, NULL, 0 );
 }
