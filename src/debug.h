8d999264e2e2ac2b33f82dd452e97a674fd5972c
Paolo Giangrandi <paolo@luccalug.it>
2011-01-04 16:16:11 +0000

    SPD_ASSERT improved, it now takes a format string and prints that one (instead of printif

--- src/debug.h
+++ src/debug.h
@@ -18,15 +18,18 @@ void print_trace( void ); // TODO: pure debugging, move in debug.c ...
 #define SPD_COLOR_GREEN		"\E[1m\E[32m"
 #define SPD_COLOR_YELLOW	"\E[1m\E[33m"
 
-#define SPD_ASSERT(x) if( ! (x) ) { print_trace(); exit(1); }
+#define SPD_ASSERT(cond, fmt, ... ) \
+	if( ! (cond) ) { \
+		SPD_ERROR( fmt "\n  (" ##cond "failed)", ##__VA_ARGS__ ); \
+	}
 
 #define SPD_BASE_ERROR_STR SPD_COLOR_YELLOW "%s:%d" SPD_COLOR_DEFAULT ": " SPD_COLOR_RED "%s()" SPD_COLOR_DEFAULT ": "
 #define SPD_BASE_ERROR_ARGS __FILE__, __LINE__, __FUNCTION__
 
-#define SPD_ERROR( FMT, ... ) \
+#define SPD_ERROR( fmt, ... ) \
 	{ \
 		char buf[1024]; \
-		printf( SPD_BASE_ERROR_STR FMT "\n", SPD_BASE_ERROR_ARGS, ##__VA_ARGS__ ); \
+		printf( SPD_BASE_ERROR_STR fmt "\n", SPD_BASE_ERROR_ARGS, ##__VA_ARGS__ ); \
 		print_trace(); \
 		exit(1); \
 	}
